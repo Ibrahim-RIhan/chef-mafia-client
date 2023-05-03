0723d05d1ae4187d201a12cc54593aba96860c87
rihan010 <ibrhimrihan000@gmail.comm>
2023-05-03 14:35:23 +0600

    spinner addded

--- src/components/Routes/PrivateRoute.jsx
+++ src/components/Routes/PrivateRoute.jsx
@@ -3,8 +3,13 @@ import { AuthContext } from '../../Providers/AuthProvider';
 import { Navigate, useLocation } from 'react-router-dom';
 
 const PrivateRoute = ({children}) => {
-    const {user} =useContext(AuthContext)
+    const {user , loading} =useContext(AuthContext)
     const location = useLocation();
+    if(loading){
+      return  <div className='flex justify-center items-center' style={{height :'100vh'}}>
+            <button className="btn loading btn-error btn-xl">loading</button>
+        </div>
+    }
     if(user){
         return children
     }
