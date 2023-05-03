0cb4a02d3f6fc2ea49f8cc731f17806cc18c0ca1
rihan010 <ibrhimrihan000@gmail.comm>
2023-05-03 17:26:50 +0600

    -final submit

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -9,7 +9,7 @@ const Login = () => {
     const navigate = useNavigate();
     const location =  useLocation();
     const from =location?.state?.from?.pathname
-    console.log(from);
+  
     const handleLogin = event => {
         event.preventDefault();
         const form = event.target;
